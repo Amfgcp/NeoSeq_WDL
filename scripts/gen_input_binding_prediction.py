3a3625a558ed2b81e252deafb2e46940021735fd
António Paulo <antonio.do.paulo@ist.utl.pt>
2019-06-13 15:09:01 +0200

    Move logic related to mass spec file out of __main__

--- scripts/gen_input_binding_prediction.py
+++ scripts/gen_input_binding_prediction.py
@@ -26,7 +26,7 @@ from Bio.Blast import NCBIXML
 
 from math import log
 
-def getShortPeptide(ALTsequence, mutpos):
+def getShortPeptide(ALTsequence, mutpos, size):
 ## subtract -1 from int(mutpos) because python is 0 based
     start = max(0, (int(mutpos) + 1  - size))
     end   = (int(mutpos) ) + size
@@ -53,128 +53,8 @@ def gap_function(x, y):  # x is gap position in seq, y is gap length
     return -0.6 + (x/51) / 2 - 0.1 # gap extension penalty
 
 ### MAIN ###
-def run(size, ms_file, ms_written):
-    blast_peps_file_name = sample_name + "_blasted_peptides_" + str(size) + "mers.fsa"
-    blast_peps_file = open(blast_peps_file_name, "w+")
-    blast_peps = []
+def run(sizes):
 
-    # NOTE: "If you just want to read or write one file see open()"
-    for line in fileinput.input(input_file):
-        words = line.split()
-        if not words[0].startswith("chr"):
-            continue
-        varID      = words[0]
-        WTpeptide  = words[1]
-        ALTpeptide = words[2]
-        # Global alignment with gap penalty of -0.5 (and -0.1 for extending it)
-        # The 'x' default means the mismastch score is 0 and match is 1
-        alignments = pairwise2.align.globalxc(WTpeptide, ALTpeptide, gap_function, gap_function, penalize_end_gaps=(False, True))
-        for ali in alignments:
-            print(format_alignment(*ali))
-        mutpos = []
-        start_WT = False
-        start_ALT = False
-        # NOTE: Check if 1st alignment has the best score
-        for i, c in enumerate(zip(alignments[0][0][0:len(ALTpeptide)], alignments[0][1])):
-            if c[0] != "-":
-                start_WT = True
-            if c[1] != "-":
-                start_ALT = True
-            if start_WT and start_ALT and c[0] != c[1]:
-                mutpos.append(i)
-
-        ms_suffix = []
-        # only slide window over a ALTpeptide if this one had differences with WTpeptide
-        for i in mutpos:
-            shortPep = getShortPeptide(ALTpeptide, i)
-            mers = getMer(shortPep, size)
-            # print ("%s\t%s\t%s\t%s\t%s" % (varID, WTpeptide, ALTpeptide, mutpos, mers))
-            # NOTE: allows repeated peptides, thus later blasted more than once
-            ms_suffix.append(str(i+1))
-            for mer in mers:
-                blast_peps_file.write(">" + varID + "\n" + mer + "\n")
-                blast_peps.append(mer)
-        if not ms_written:
-            ms_file.write(varID + "_M" + ",".join(ms_suffix) + "\t" + ALTpeptide + "\n")
-
-    blast_peps_file.close()
-    ms_file.close()
-    ms_written = True
-
-    ### BLAST ###
-    xml_file_name = sample_name + "_blast_output_" + str(size) + "mers_swissprot9606.xml"
-    # TODO: create param for DB and create file name for XML file accordingly
-    blastp_cline = NcbiblastpCommandline( \
-                    query=blast_peps_file_name, \
-                    task="blastp-short", \
-                    db="/exports/path-demiranda/usr/amfgcp/databases/ncbi/v5/generated/swissprot_taxid_9606/swissprot_taxid_9606", \
-                    outfmt=5, \
-                    out=xml_file_name, \
-                    remote=False)
-    print("*****    BLAST   *****")
-    print("Executing:\n\t", blastp_cline)
-    stdout, stderr = blastp_cline()
-    print("stdout: ", stdout)
-    print("stderr: ", stderr)
-    print("***** END OF BLAST *****")
-
-    result_handle = open(xml_file_name)
-    blast_records = NCBIXML.parse(result_handle)
-
-    neoantigen_candidates = {}
-
-    decoding_file_name = sample_name + "_peptides_" + str(size) + "mers_decoding.txt"
-    decoding_file = open(decoding_file_name, "w+")
-
-    perfect_hits_file_name = sample_name + "_perfect_hits_" + str(size) + "mer.txt"
-    perfect_hits_file = open(perfect_hits_file_name, "w+")
-
-    for i, blast_record in enumerate(blast_records):
-        print("/////////////////////////////", blast_record.query)
-        print(i, " ", blast_peps[i])
-        candidate = True
-        for alignment in blast_record.alignments:
-            print("****Alignment****")
-            print("sequence:", alignment.title)
-            for hsp in alignment.hsps:
-                print(hsp.query)
-                print(hsp.match)
-                print(hsp.sbjct)
-                print("Identities: ", hsp.identities)
-                print("Alignment Lenght: ", hsp.align_length)
-                print("Size: ", size)
-                # Disregard perfect hits. `align_length` is also checked because of
-                # possible gaps in the alignment.
-                if (size == hsp.identities == hsp.align_length):
-                    print("FOUND PERFECT HIT:\t" + blast_peps[i])
-                    candidate = False
-                    perfect_hits_file.write(blast_peps[i] + "\t" + blast_record.query + "\t" + alignment.title + "\n")
-                    break
-            if not candidate:
-                break
-        if candidate:
-            decoding_file.write(str(i) + "\t" + blast_record.query + "\n")
-            try:
-                neoantigen_candidates[blast_peps[i]].add(i)
-            except KeyError: # key not in dictionary
-                neoantigen_candidates[blast_peps[i]] = {i}
-
-    decoding_file.close()
-    perfect_hits_file.close()
-
-    output_file_name = sample_name + "_peptides_" + str(size) + "mers.fsa"
-    # Note that the same peptide generated from two different variants will be repeated
-    with open(output_file_name, "w+") as output_file:
-        for pep, varIDs in neoantigen_candidates.items():
-            for _id in varIDs:
-                output_file.write(">" + str(_id) + "\n" + pep + "\n")
-
-if __name__ == '__main__':
-    arguments = docopt(__doc__, version='Gen Inputs Bind Pred 1.0')
-    sizes = [int(i) for i in arguments["-p"].split(",")] 
-    input_file = arguments["-f"]
-    sample_name = input_file.split(".")[0]
-    
     mass_spec_folder_name = "51aa/"
     if not os.path.exists(mass_spec_folder_name):
         os.mkdir(mass_spec_folder_name)
@@ -184,4 +64,127 @@ if __name__ == '__main__':
     mass_spec_written = False
 
     for size in sizes:
-        run(size, mass_spec_file, mass_spec_written)
+        blast_peps_file_name = sample_name + "_blasted_peptides_" + str(size) + "mers.fsa"
+        blast_peps_file = open(blast_peps_file_name, "w+")
+        blast_peps = []
+
+        # NOTE: "If you just want to read or write one file see open()"
+        for line in fileinput.input(input_file):
+            words = line.split()
+            if not words[0].startswith("chr"):
+                continue
+            varID      = words[0]
+            WTpeptide  = words[1]
+            ALTpeptide = words[2]
+            # Global alignment with gap penalty of -0.5 (and -0.1 for extending it)
+            # The 'x' default means the mismastch score is 0 and match is 1
+            alignments = pairwise2.align.globalxc(WTpeptide, ALTpeptide, gap_function, gap_function, penalize_end_gaps=(False, True))
+            # for ali in alignments:
+            #     print(format_alignment(*ali))
+            mutpos = []
+            start_WT = False
+            start_ALT = False
+            # NOTE: Check if 1st alignment has the best score
+            for i, c in enumerate(zip(alignments[0][0][0:len(ALTpeptide)], alignments[0][1])):
+                if c[0] != "-":
+                    start_WT = True
+                if c[1] != "-":
+                    start_ALT = True
+                if start_WT and start_ALT and c[0] != c[1]:
+                    mutpos.append(i)
+
+            mass_spec_suffix = []
+            # only slide window over a ALTpeptide if this one had differences with WTpeptide
+            for i in mutpos:
+                shortPep = getShortPeptide(ALTpeptide, i, size)
+                mers = getMer(shortPep, size)
+                # print ("%s\t%s\t%s\t%s\t%s" % (varID, WTpeptide, ALTpeptide, mutpos, mers))
+                # NOTE: allows repeated peptides, thus later blasted more than once
+                mass_spec_suffix.append(str(i+1))
+                for mer in mers:
+                    blast_peps_file.write(">" + varID + "\n" + mer + "\n")
+                    blast_peps.append(mer)
+
+            if not mass_spec_written:
+                mass_spec_file.write(varID + "_M" + ",".join(mass_spec_suffix) + "\t" + ALTpeptide + "\n")
+
+        blast_peps_file.close()
+        mass_spec_file.close()
+        mass_spec_written = True
+
+        ### BLAST ###
+        xml_file_name = sample_name + "_blast_output_" + str(size) + "mers_swissprot9606.xml"
+        # TODO: create param for DB and create file name for XML file accordingly
+        blastp_cline = NcbiblastpCommandline( \
+                        query=blast_peps_file_name, \
+                        task="blastp-short", \
+                        db="/exports/path-demiranda/usr/amfgcp/databases/ncbi/v5/generated/swissprot_taxid_9606/swissprot_taxid_9606", \
+                        outfmt=5, \
+                        out=xml_file_name, \
+                        remote=False)
+        print("*****    BLAST   *****")
+        print("Executing:\n\t", blastp_cline)
+        stdout, stderr = blastp_cline()
+        print("stdout: ", stdout)
+        print("stderr: ", stderr)
+        print("***** END OF BLAST *****")
+
+        result_handle = open(xml_file_name)
+        blast_records = NCBIXML.parse(result_handle)
+
+        neoantigen_candidates = {}
+
+        decoding_file_name = sample_name + "_peptides_" + str(size) + "mers_decoding.txt"
+        decoding_file = open(decoding_file_name, "w+")
+
+        perfect_hits_file_name = sample_name + "_perfect_hits_" + str(size) + "mer.txt"
+        perfect_hits_file = open(perfect_hits_file_name, "w+")
+
+        for i, blast_record in enumerate(blast_records):
+            print("/////////////////////////////", blast_record.query)
+            print(i, " ", blast_peps[i])
+            candidate = True
+            for alignment in blast_record.alignments:
+                print("****Alignment****")
+                print("sequence:", alignment.title)
+                for hsp in alignment.hsps:
+                    print(hsp.query)
+                    print(hsp.match)
+                    print(hsp.sbjct)
+                    print("Identities: ", hsp.identities)
+                    print("Alignment Lenght: ", hsp.align_length)
+                    print("Size: ", size)
+                    # Disregard perfect hits. `align_length` is also checked because of
+                    # possible gaps in the alignment.
+                    if (size == hsp.identities == hsp.align_length):
+                        print("FOUND PERFECT HIT:\t" + blast_peps[i])
+                        candidate = False
+                        perfect_hits_file.write(blast_peps[i] + "\t" + blast_record.query + "\t" + alignment.title + "\n")
+                        break
+                if not candidate:
+                    break
+            if candidate:
+                decoding_file.write(str(i) + "\t" + blast_record.query + "\n")
+                try:
+                    neoantigen_candidates[blast_peps[i]].add(i)
+                except KeyError: # key not in dictionary
+                    neoantigen_candidates[blast_peps[i]] = {i}
+
+        decoding_file.close()
+        perfect_hits_file.close()
+
+        output_file_name = sample_name + "_peptides_" + str(size) + "mers.fsa"
+        # Note that the same peptide generated from two different variants will be repeated
+        with open(output_file_name, "w+") as output_file:
+            for pep, varIDs in neoantigen_candidates.items():
+                for _id in varIDs:
+                    output_file.write(">" + str(_id) + "\n" + pep + "\n")
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Gen Inputs Bind Pred 1.0')
+    sizes = [int(i) for i in arguments["-p"].split(",")]
+    input_file = arguments["-f"]
+    sample_name = input_file.split(".")[0]
+
+    run(sizes)
